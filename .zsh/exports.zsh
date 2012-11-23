88d9f975bd7a4835d1712577c061140aeb1744e5
Labi Kyo <labikyo@gmail.com>
2012-11-24 00:29:03 +0800

    Add  in env

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,6 +1,7 @@
 # exports
+export EDITOR=vim
 export PATH=/usr/local/sbin:/usr/local/bin:$PATH
 export ARCHFLAGS='-arch x86_64'
-export NODE_ENV='dev'
+export NODE_ENV='development'
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
