15e4baf7f27f545e96f06d0e2e047ddfa256af37
Labi Kyo <labikyo@gmail.com>
2012-12-21 16:55:56 +0800

    Update exports on mac TODO: update for linux

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,7 +1,8 @@
 # exports
 export EDITOR=vim
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:$PATH
 export ARCHFLAGS='-arch x86_64'
 export NODE_ENV='development'
+export NODE_PATH='/usr/local/share/npm/lib/node_modules'
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
