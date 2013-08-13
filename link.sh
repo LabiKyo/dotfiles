14906c13148b42898a02d24474168d121e40db24
Labi Kyo <labikyo@gmail.com>
2013-08-13 23:55:30 +0800

    Change to oh-my-zsh

--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@ ln ./.vim/.gitconfig ./
 ln ./.vim/.vimrc ./
 ln ./.vim/.zshrc ./
 ln -s ./.vim/.zsh ./.zsh
+ln -s ./.vim/.oh-my-zsh ./.oh-my-zsh/
 ln ./.vim/.tmux.conf ./
 ln ./.vim/.pentadactylrc ./.pentadactylrc
 ln -s ./.vim/.pentadactyl ./.pentadactyl
