851846d431fa0db0fbcb363bf9b72647a5768479
Labi Kyo <labikyo@gmail.com>
2014-07-19 15:31:40 +0800

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 cd ~
-ln ./.vim/.gitignore ./
-ln ./.vim/.gitconfig ./
-ln ./.vim/.vimrc ./
-ln ./.vim/.vimrc.user ./
-ln ./.vim/.vimrc.keymap ./
-ln ./.vim/.vimrc.plugin ./
-ln ./.vim/.zshrc ./
-ln -s ./.vim/.zsh ./.zsh
-ln -s ./.vim/.oh-my-zsh ./.oh-my-zsh/
-ln ./.vim/.tmux.conf ./
-ln ./.vim/.pentadactylrc ./.pentadactylrc
-ln -s ./.vim/.pentadactyl ./.pentadactyl
+ln -sf ./.vim/.gitignore ./
+ln -sf ./.vim/.gitconfig ./
+ln -sf ./.vim/.vimrc ./
+ln -sf ./.vim/.vimrc.user ./
+ln -sf ./.vim/.vimrc.keymap ./
+ln -sf ./.vim/.vimrc.plugin ./
+ln -sf ./.vim/.zshrc ./
+ln -sf ./.vim/.tmux.conf ./
+ln -sf ./.vim/.redis.conf ./
+ln -sf ./.vim/.gemrc ./
 
-ln ./.vim/.redis.conf ./
+ln -sfh ./.vim/.zsh ./.zsh
+ln -sfh ./.vim/.oh-my-zsh ./.oh-my-zsh
