f455e868c65267a355187ee8b4794485166e6ef1
Labi Kyo <labikyo@gmail.com>
2014-12-15 15:30:50 +0800

    Add jsx.vim plugin and remove solarized colorscheme

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,8 +1,9 @@
 # exports
 export EDITOR=vim
-export PATH=~/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:$PATH
 export ARCHFLAGS='-arch x86_64'
 export NODE_ENV='development'
-export NODE_PATH='/usr/local/share/npm/lib/node_modules'
+export NODE_PATH='/usr/local/share/npm/lib/node_modules:/usr/local/lib/node_modules'
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+
+export PATH=~/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:$PATH
