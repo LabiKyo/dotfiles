a3dd392d48b7fc0b088e5c4e634b3c787bd82a8c
Labi Kyo <labikyo@gmail.com>
2015-08-11 15:25:52 +0800

    Add node taobao mirror

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,5 +5,7 @@ export NODE_ENV='development'
 export NODE_PATH='/usr/local/share/npm/lib/node_modules:/usr/local/lib/node_modules'
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
+export NVM_IOJS_ORG_MIRROR=http://npm.taobao.org/mirrors/iojs
 
 export PATH=~/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:$PATH
